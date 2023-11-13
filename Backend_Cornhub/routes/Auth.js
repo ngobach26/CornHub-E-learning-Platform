619317f5a94e80a091ef846a5ae5c06d981d326e
Bach Ngo <ngobach26@gmail.com>
2023-11-14 01:07:11 +0700

    small refactor on dir name and file name

--- Backend_Cornhub/routes/Auth.js
+++ Backend_Cornhub/routes/Auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, login} =  require('../Controllers/AuthControllers');
+const { signup, login} =  require('../controllers/authControllers');
 const requireAuth = require("../middleware/requireAuth");
 
 // router.use(requireAuth);
