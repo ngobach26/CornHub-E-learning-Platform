6296a283a52bd64480e3f69f91425a30ee392466
Viet Nguyen <130389965+JimmyNguyen11@users.noreply.github.com>
2024-01-06 13:36:00 +0700

    edit admin api

--- Backend_Cornhub/routes/Admin.js
+++ Backend_Cornhub/routes/Admin.js
@@ -4,9 +4,9 @@ const adminController = require('../controller/adminController');
 const requireAuth = require('../middleware/requireAuth'); 
 const isAdmin = require('../middleware/isAdmin');
 
-router.get('/listusers', requireAuth, isAdmin, adminController.listusers);
-router.get('/listcourses', requireAuth, isAdmin, adminController.listcourses);
-router.get('/deletecourse', requireAuth, isAdmin, adminController.deletecourse);
-router.patch('/acceptcourse/:id', requireAuth, isAdmin, adminController.acceptcourse);
-router.patch('/denycourse/:id', requireAuth, isAdmin, adminController.denycourse);
+router.get('/users', requireAuth, isAdmin, adminController.listusers);
+router.get('/courses', requireAuth, isAdmin, adminController.listcourses);
+router.delete('/courses/:id', requireAuth, isAdmin, adminController.deletecourse);
+router.patch('/courses/:id', requireAuth, isAdmin, adminController.acceptcourse);
+router.patch('/courses/:id', requireAuth, isAdmin, adminController.denycourse);
 module.exports = router;
